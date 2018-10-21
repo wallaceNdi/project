5067ac38059b6e4fde8351f4114b5142b5411da7
wallacefon <42949636+wallacefon@users.noreply.github.com>
2018-10-21 02:43:04 +0200

    Add files via upload

---
+++ Playlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
+
+class Playlist extends React.Component {
+ 
+
+   render(){
+     return(
+       <div className="Playlist">
+  <input defaultValue ="New Playlist"/>
+  <TrackList track ={this.props.playlistTracks} />
+  <a className="Playlist-save">SAVE TO SPOTIFY</a>
+</div>
+     )
+   }
+}
+export default Playlist;
